2e1b5f02a302155e834510baaefbc359f5d0696a
Saurabh Tiwari <tsaurabh2001@gmail.com>
2021-05-12 16:06:23 +0530

    compatible with all OS

--- main.py
+++ main.py
@@ -42,27 +42,27 @@ try:
     org = os.path.join(des,"Original")
     os.mkdir(org,0o666)
 
-    cntdupe, cntorg = 0
+    cntdupe, cntorg = 0, 0
 
     for img1 in images:
         if (not check[images.index(img1)]) and images.index(img1) != len(images)-1:
-            shutil.copy(src+'\\'+img1, org)
+            shutil.copy(os.path.join(src,img1), org)
             cntorg += 1
-            temp1 = cv2.imread(src+'\\'+img1, 0)
+            temp1 = cv2.imread(os.path.join(src,img1), 0)
             rimg1 = cv2.resize(temp1, (8,8), interpolation=cv2.INTER_AREA)
             imgdict[str(np.array(rimg1).flatten())] = 1
 
             for img2 in images[images.index(img1)+1:]:
-                temp2 = cv2.imread(src+'\\'+img2, 0)
+                temp2 = cv2.imread(os.path.join(src,img2), 0)
                 rimg2 = cv2.resize(temp2, (8,8), interpolation=cv2.INTER_AREA)
                 if args.strict:
                     if isStrictdupe(rimg1, rimg2):
-                        shutil.copy(src+'\\'+img2, dupes)
+                        shutil.copy(os.path.join(src,img2), dupes)
                         cntdupe += 1
                         check[images.index(img2)] = True
                 else:
                     if isSim(rimg1, rimg2):
-                        shutil.copy(src+'\\'+img2, dupes)
+                        shutil.copy(os.path.join(src,img2), dupes)
                         cntdupe += 1
                         check[images.index(img2)] = True
 
