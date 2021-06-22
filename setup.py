431aca12a7572a75ade8d2fb940bfda9de3f10dc
Saurabh Tiwari <tsaurabh2001@gmail.com>
2021-06-22 15:33:19 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Findupe',
+    version='1.0',
+    author='Saurabh Tiwari',
+    packages=find_packages(),
+    description='Finding similar looking images using structural similarity',
+    url='https://github.com/meispi/Findupe'
+)
