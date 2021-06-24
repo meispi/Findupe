08930fbc1027d529dfcc7551f3d7d9769624ccde
Saurabh Tiwari <tsaurabh2001@gmail.com>
2021-06-24 17:48:45 +0530

    error fixed

--- findupe.py
+++ findupe.py
@@ -50,40 +50,40 @@ try:
         os.mkdir(dupes,0o666)
         os.mkdir(org,0o666)
 
-    except FileExistsError:
-        print("There already exists 2 folders called Dupes and Original in this directory, please make sure to remove them and try again")
+        cntdupe, cntorg = 0, 0
 
-    cntdupe, cntorg = 0, 0
-
-    for (i,img1) in enumerate(images):
-        if (not check[img1]) and i != len(images)-1:
-            shutil.copy(os.path.join(src,img1), org)
-            cntorg += 1
-            temp1 = cv2.imread(os.path.join(src,img1), 0)
-            rimg1 = cv2.resize(temp1, (8,8), interpolation=cv2.INTER_AREA)
-            if args.strict:
-                imgdict[flat(np.array(rimg1))] = 1
-
-            for img2 in images[i+1:]:
-                temp2 = cv2.imread(os.path.join(src,img2), 0)
-                rimg2 = cv2.resize(temp2, (8,8), interpolation=cv2.INTER_AREA)
+        for (i,img1) in enumerate(images):
+            if (not check[img1]) and i != len(images)-1:
+                shutil.copy(os.path.join(src,img1), org)
+                cntorg += 1
+                temp1 = cv2.imread(os.path.join(src,img1), 0)
+                rimg1 = cv2.resize(temp1, (8,8), interpolation=cv2.INTER_AREA)
                 if args.strict:
-                    if isStrictdupe(rimg1, rimg2):
-                        shutil.copy(os.path.join(src,img2), dupes)
-                        cntdupe += 1
-                        check[str(img2)] = True
-                else:
-                    if isSim(rimg1, rimg2):
-                        shutil.copy(os.path.join(src,img2), dupes)
-                        cntdupe += 1
-                        check[str(img2)] = True
+                    imgdict[flat(np.array(rimg1))] = 1
 
-        print("[INFO] images processed {}/{}".format(i+1,len(images)), end='')
-        print(f"\t dupes: {cntdupe}, org: {cntorg}")
+                for img2 in images[i+1:]:
+                    temp2 = cv2.imread(os.path.join(src,img2), 0)
+                    rimg2 = cv2.resize(temp2, (8,8), interpolation=cv2.INTER_AREA)
+                    if args.strict:
+                        if isStrictdupe(rimg1, rimg2):
+                            shutil.copy(os.path.join(src,img2), dupes)
+                            cntdupe += 1
+                            check[str(img2)] = True
+                    else:
+                        if isSim(rimg1, rimg2):
+                            shutil.copy(os.path.join(src,img2), dupes)
+                            cntdupe += 1
+                            check[str(img2)] = True
+
+            print("[INFO] images processed {}/{}".format(i+1,len(images)), end='')
+            print(f"\t dupes: {cntdupe}, org: {cntorg}")
         
 
-    print(f'No. of dupes found: {cntdupe}')
-    print(f'No. of originals found: {cntorg}')
+        print(f'No. of dupes found: {cntdupe}')
+        print(f'No. of originals found: {cntorg}')
+    
+    except FileExistsError:
+        print("There already exists 2 folders called Dupes and Original in this directory, please make sure to remove them and try again")
 
 except FileNotFoundError:
     exit('The specified directory wasn\'t found, please provide the full path!')
